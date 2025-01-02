a7b34d5c77df119e2f7340d2e6d800ddd8b8bbfc
Minxxey <lina.staudt@googlemail.com>
2025-01-02 10:57:30 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/jobs', function() {
+    $title = 'Available Jobs';
+    $jobs = [
+        'web developer',
+        'db admin', 'software engineer'
+    ];
+
+    return view('jobs.index', compact('title', 'jobs'));
+})->name('jobs');
+
+Route::get('/jobs/create', function() {
+    return view('jobs.create');
+})->name('jobs.create');
+
+
