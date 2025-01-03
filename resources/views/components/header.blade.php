2211b8a6b99197f4590f8cbe804f431d64e6af33
Minxxey <lina.staudt@googlemail.com>
2025-01-03 16:16:40 +0100

    tm-5: - added mobile menu functionality - reworked header to use nv links - added components to layout & index - added hero styling

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,52 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/jobs">Jobs</a>
-    <a href="/jobs/create">Create Jobs</a>
-</nav>
+<header class="bg-blue-900 text-white p-4">
+    <div class="container mx-auto flex justify-between items-center">
+        <h1 class="text-3xl font-semibold">
+            <a href="{{url('/')}}">Workopia</a>
+        </h1>
+        <nav class="hidden md:flex items-center space-x-4">
+            <x-nav-link url="/" :active="request()->is('/')">
+                Home
+            </x-nav-link>
+            <x-nav-link url="/jobs" :active="request()->is('jobs')">
+                Jobs
+            </x-nav-link>
+            <x-nav-link url="/jobs/saved" :active="request()->is('/obs/saved')">
+                Saved Jobs
+            </x-nav-link>
+            <x-nav-link url="/login" :active="request()->is('login')">
+                Login
+            </x-nav-link>
+            <x-nav-link url="/register" :active="request()->is('register')">
+                Register
+            </x-nav-link>
+            <x-button-link url="/jobs/create" icon="edit">Create Job</x-button-link>
+            <x-button-link url="/dashboard" icon="gauge">Dashboard</x-button-link>
+        </nav>
+        <button
+            id="hamburger"
+            class="text-white md:hidden flex items-center"
+        >
+            <i class="fa fa-bars text-2xl"></i>
+        </button>
+    </div>
+    <!-- Mobile Menu -->
+    <nav
+        id="mobile-menu"
+        class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
+    >
+        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">
+            Jobs
+        </x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile="true">
+            Saved Jobs
+        </x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile="true">
+            Login
+        </x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')" :mobile="true">
+            Register
+        </x-nav-link>
+        <x-button-link url="/jobs/create" icon="edit" :block="true">Create Job</x-button-link>
+        <x-button-link url="/dashboard" icon="gauge" :block="true">Dashboard</x-button-link>
+    </nav>
+</header>
