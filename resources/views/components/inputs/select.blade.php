61bad684411a7671ffdc876b2d497ca9db732d3f
Minxxey <lina.staudt@googlemail.com>
2025-01-08 16:28:01 +0100

    tm-8 (inputs): - added select input component

---
+++ resources/views/components/inputs/select.blade.php
@@ -0,0 +1,28 @@
+@props([
+    'id',
+    'name',
+    'label' => null,
+    'value' => '',
+    'options' => [] //assoc array
+])
+<div class="mb-4">
+    @if($label)
+        <label class="block text-gray-700" for="{{$id}}"
+        >{{$label}}</label
+        >
+    @endif
+    <select
+        id="{{$id}}"
+        name="{{$name}}"
+        class="w-full px-4 py-2 border rounded focus:outline-none @error($name) border-red-500 @enderror"
+    >
+        @foreach($options as $optionValue => $optionLabel)
+            <option value="$optionValue" {{old($name, $value) == $optionValue ? 'selected' : ''}}>
+                {{$optionLabel}}
+            </option>
+        @endforeach
+    </select>
+    @error($name)
+    <p class="text-sm text-red-500 mt-1">{{$message}}</p>
+    @enderror
+</div>
